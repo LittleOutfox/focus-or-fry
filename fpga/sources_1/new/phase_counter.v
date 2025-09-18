6123a5569e87c3192d764bb536c0b152658ca95a
Ethan Tiong <ethanwtiong@outlook.com>
2025-09-18 16:49:55 -0400

    This code is booty cheeks but I can't solve it till an FPGA expert comes along

--- fpga/sources_1/new/phase_counter.v
+++ fpga/sources_1/new/phase_counter.v
@@ -2,7 +2,8 @@ module phase_counter (
     input baud_en,
     input clk,
     input phase_arm,
-    output reg center_tick = 0
+    output reg center_tick = 0,
+    output reg first_tick = 0
 );
     parameter integer OVERSAMPLE = 16;
     localparam integer CENTER = OVERSAMPLE/2; // Lean towards safe side of bit 
@@ -12,14 +13,18 @@ module phase_counter (
 
     always@(posedge clk) begin
         center_tick <= 0;
+        first_tick <= 0;
 
         if(phase_arm) begin
             counter <= 0;
         end else begin
             if(baud_en) begin 
                 if (counter == CENTER) begin
-                    counter <= counter + 1;
                     center_tick <= 1;
+                    counter <= counter + 1;
+                end else if (counter == 0) begin
+                    first_tick <= 1;
+                    counter <= counter + 1;
                 end else if (counter == OVERSAMPLE-1) begin
                     counter <= 0;
                 end else begin 
--- fpga/sources_1/new/uart_tx.v
+++ fpga/sources_1/new/uart_tx.v
@@ -3,16 +3,17 @@ module uart_tx (
     input start,
     input baud_tick,
     input reset,
-    input [7:0] data,
+    input [FRAME_BITS - 1:0] data,
     output reg tx_out,
     output reg tx_status //high = busy, low = ready
 );
-
-  localparam [1:0] IDLE = 2'b00, START = 2'b01, DATA = 2'b10, STOP = 2'b11;
   parameter integer FRAME_BITS = 8;
+  parameter integer OVERSAMPLE = 16;
+  localparam [1:0] IDLE = 2'b00, START = 2'b01, DATA = 2'b10, STOP = 2'b11;
   reg [1:0] state;
   reg [1:0] next_state = 0;
   reg [$clog2(FRAME_BITS) - 1:0] bit_index = 0;
+  reg [$clog2(OVERSAMPLE) - 1:0] sample_index = 0;
 
   // Async reset w/ transition logic
   always @(posedge clk or posedge reset) begin
@@ -27,14 +28,80 @@ module uart_tx (
   always @(*) begin
     next_state = state;
     case (state)
-    default: next_state = state;
+      IDLE : begin
+        if (start) begin
+          next_state = START;
+        end else begin
+          next_state = IDLE;
+        end
+      end
+
+      START : begin
+        if (baud_tick) begin
+          if (sample_index < OVERSAMPLE) begin
+            sample_index = sample_index + 1;
+          end else begin
+            sample_index = 0;
+            next_state = DATA;
+          end
+        end else begin
+          sample_index = sample_index;
+          next_state = state;
+        end
+      end
+
+      DATA : begin
+        if (baud_tick) begin
+          if (sample_index < OVERSAMPLE) begin // KEEP TRANSMITTING SAME BIT
+            sample_index = sample_index + 1;
+          end else begin // TIME TO MOVE TO NEXT BIT
+            sample_index = 0;
+            if (bit_index <  FRAME_BITS) begin 
+              bit_index = bit_index + 1;
+            end else begin
+              bit_index = 0;
+              next_state = STOP;
+            end
+          end
+        end else begin
+          sample_index = sample_index;
+          bit_index = bit_index;
+          next_state = state;
+        end
+      end
+
+      STOP : begin
+        if (baud_tick) begin
+          if (sample_index < OVERSAMPLE) begin
+            sample_index = sample_index + 1;
+          end else begin
+            sample_index = 0;
+            next_state = DATA;
+          end
+        end else begin
+          sample_index = sample_index;
+          next_state = state;
+        end
+      end
+
+      default: next_state = state;
     endcase
   end
 
-  // Synchronous 
+  // Synchronous Output Logic
   always @(posedge clk or posedge reset) begin
     if (reset) begin
+      tx_out <= 1;
+      tx_status <= 0;
+      bit_index <= 0;
     end else begin
+      case (state)
+        IDLE : tx_out <= 1;
+        START :tx_out <= 0;
+        DATA : tx_out <= data[bit_index];
+        STOP : tx_out <= 1;
+      endcase
     end
+  end
 
 endmodule
